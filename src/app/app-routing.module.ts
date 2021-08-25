bf5adcb9d119c7bd29bd0ea762585d8bb4d7c115
John Fernando  Castaño Sena <j.castano@sisalril.gov.do>
2021-08-25 16:31:36 -0400

    Primer diseño de la pantalla de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ListaPeliculasComponent } from './lista-peliculas/lista-peliculas.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'lista-peliculas', pathMatch: 'full'},
+  {path: 'lista-peliculas', component: ListaPeliculasComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
